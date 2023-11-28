ecf13f038fc11f29c130f65bac1fdc87cff1be3b
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-11-28 07:01:34 +0100

    frontend code

---
+++ src/components/VideoUpload.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+function VideoUpload() {
+  const [file, setFile] = useState<File | undefined>();
+  return <div>videoUpload</div>;
+}
+
+export default VideoUpload;
