2a9fddef7337bf98fe649faff43c1a846e53affa
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-11-29 06:43:29 +0100

    add getvideostream function

--- src/pages/api/videos.ts
+++ src/pages/api/videos.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import busboy from "busboy";
 import fs from "fs";
+
 export const config = {
   api: {
     bodyParser: false,
@@ -25,6 +26,41 @@ function uploadVideoStream(req: NextApiRequest, res: NextApiResponse) {
   return;
 }
 
+const CHUNK_SIZE_IN_BYTES = 1000000; //1mb?
+
+function getVideoStream(req: NextApiRequest, res: NextApiResponse) {
+  const range = req.headers.range;
+
+  if (!range) {
+    return res.status(400).send("No range");
+  }
+
+  const videoId = req.query.videoId;
+  const videoPath = `./videos/${videoId}.mp4`;
+  const videoSizeInBytes = fs.statSync(videoPath).size;
+  const chunkStart = Number(range.replace(/\D/g, ""));
+  const chunkEnd = Math.min(
+    chunkStart + CHUNK_SIZE_IN_BYTES,
+    videoSizeInBytes - 1,
+  );
+
+  const contentLength = chunkEnd - chunkStart + 1;
+
+  const headers = {
+    "Content-Range": `bytes ${chunkStart}- ${chunkEnd}/${videoSizeInBytes}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+  res.writeHead(206, headers);
+  const videoStream = fs.createReadStream(videoPath, {
+    start: chunkStart,
+    end: chunkEnd,
+  });
+  videoStream.pipe(res);
+}
+// do some error checking to make sure a video exists and if not return a 404
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -32,7 +68,7 @@ export default function handler(
   const method = req.method;
 
   if (method === "GET") {
-    return res.status(200).json({ name: "bruh" });
+    return getVideoStream(req, res);
   }
   if (method === "POST") {
     return uploadVideoStream(req, res);
