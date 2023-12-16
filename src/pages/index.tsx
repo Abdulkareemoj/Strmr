9b02306892c5f0a5afe9f429e82a2ad395785cbd
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-16 07:54:48 +0100

    trying to figure out this auth thing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import { auth } from "@/lib/auth";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>hello</main>;
+  const { user, session, loading } = auth.useSession();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!session) {
+    return (
+      <div>
+        Not signed in. <br />
+        <button onClick={() => auth.signIn()}>Sign in</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      Signed in as {user.email} <br />
+      <button onClick={() => auth.signOut()}>Sign out</button>
+    </div>
+  );
 }
