a852410535f03e824c9ed296a8d2144bf9881b30
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-16 07:57:59 +0100

    dynamic link

--- src/pages/videos/[videoId].tsx
+++ src/pages/videos/[videoId].tsx
@@ -2,7 +2,7 @@ import VideoPlayer from "@/components/VideoPlayer";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 
-function VideoPage() {
+export default function VideoPage() {
   const router = useRouter();
   const { videoId } = router.query as { videoId: string };
 
@@ -15,4 +15,18 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   };
 };
 
-export default VideoPage;
+// import { useState } from 'react'
+// import { auth } from "../auth" // Replace with the actual path to your auth.ts file
+
+// export default function HomePage() {
+//   const [videoId, setVideoId] = useState('')
+
+//   return (
+//     <div>
+//       <input type="text" value={videoId} onChange={e => setVideoId(e.target.value)} />
+//       <video controls>
+//         <source src={`/api/videos/stream?id=${videoId}`} type="video/mp4" />
+//       </video>
+//     </div>
+//   )
+// }
