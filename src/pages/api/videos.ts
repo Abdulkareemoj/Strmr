24f61a3860f24a49dc4d82fcaf9c96c687a7cdcd
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-16 07:59:02 +0100

    Ryoiki Tenkai: Absolute Spaghettifiation Code

--- src/pages/api/videos.ts
+++ src/pages/api/videos.ts
@@ -6,6 +6,9 @@ import fileType from "file-type";
 import { pipeline } from "stream";
 import util from "util";
 import cloudinary from "cloudinary";
+import { auth } from "@/lib/auth";
+import { PrismaClient } from "@prisma/client";
+import fetch from "node-fetch";
 
 export const config = {
   api: {
@@ -19,16 +22,57 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const pipelinePromise = util.promisify(pipeline);
+//Upload to cloudinary
 
-function uploadVideoStream(req: NextApiRequest, res: NextApiResponse) {
+// const pipelinePromise = util.promisify(pipeline);
+
+async function uploadVideoStream(req: NextApiRequest, res: NextApiResponse) {
+  const session = await auth.getSession({ req });
+
+  if (!session) {
+    res.status(401).json({ message: "Not signed in" });
+    return;
+  }
+
+  if (session.user.role !== "admin") {
+    res.status(403).json({ message: "Not authorized" });
+    return;
+  }
   const bb = busboy({ headers: req.headers });
 
-  bb.on("file", (_, file, info) => {
+  bb.on("file", async (_, file, info) => {
     const fileName = info.filename;
     const filePath = `./videos/${fileName}`;
     const stream = fs.createWriteStream(filePath);
     file.pipe(stream);
+    // In your upload function, after uploading to Cloudinary
+    cloudinary.v2.uploader.upload(
+      filePath,
+      { resource_type: "video" },
+      async (error, result) => {
+        if (error) {
+          console.error("Upload to Cloudinary failed:", error);
+        } else {
+          console.log(
+            "Upload to Cloudinary successful. Video URL:",
+            result.url,
+          );
+
+          // Create a new video record and save it to the database
+          const video = await prisma.video.create({
+            data: {
+              title: "My Video",
+              description: "A great video",
+              videoId: result.public_id,
+              public: true, // Set this based on the user's input
+              // Add any other fields you need
+            },
+          });
+
+          console.log("Video saved to database successfully:", video);
+        }
+      },
+    );
   });
 
   bb.on("close", () => {
@@ -39,50 +83,104 @@ function uploadVideoStream(req: NextApiRequest, res: NextApiResponse) {
   return;
 }
 
-const CHUNK_SIZE_IN_BYTES = 1000000; //1mb?
+// const CHUNK_SIZE_IN_BYTES = 1000000; //1mb?
+// function getVideoStream(req: NextApiRequest, res: NextApiResponse) {
+//   const range = req.headers.range;
 
-function getVideoStream(req: NextApiRequest, res: NextApiResponse) {
-  const range = req.headers.range;
+//   if (!range) {
+//     return res.status(400).send("No range");
+//   }
 
-  if (!range) {
-    return res.status(400).send("No range");
-  }
+//   const videoId = req.query.videoId;
+//   const videoPath = `./videos/${videoId}.mp4`;
+//   const videoSizeInBytes = fs.statSync(videoPath).size;
+//   const chunkStart = Number(range.replace(/\D/g, ""));
+//   const chunkEnd = Math.min(
+//     chunkStart + CHUNK_SIZE_IN_BYTES,
+//     videoSizeInBytes - 1,
+//   );
 
-  const videoId = req.query.videoId;
-  const videoPath = `./videos/${videoId}.mp4`;
-  const videoSizeInBytes = fs.statSync(videoPath).size;
-  const chunkStart = Number(range.replace(/\D/g, ""));
-  const chunkEnd = Math.min(
-    chunkStart + CHUNK_SIZE_IN_BYTES,
-    videoSizeInBytes - 1,
-  );
+//   const contentLength = chunkEnd - chunkStart + 1;
 
-  const contentLength = chunkEnd - chunkStart + 1;
-
-  const headers = {
-    "Content-Range": `bytes ${chunkStart}- ${chunkEnd}/${videoSizeInBytes}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
-  res.writeHead(206, headers);
-  const videoStream = fs.createReadStream(videoPath, {
-    start: chunkStart,
-    end: chunkEnd,
-  });
-  videoStream.pipe(res);
-}
+//   const headers = {
+//     "Content-Range": `bytes ${chunkStart}- ${chunkEnd}/${videoSizeInBytes}`,
+//     "Accept-Ranges": "bytes",
+//     "Content-Length": contentLength,
+//     "Content-Type": "video/mp4",
+//   };
+//   res.writeHead(206, headers);
+//   const videoStream = fs.createReadStream(videoPath, {
+//     start: chunkStart,
+//     end: chunkEnd,
+//   });
+//   videoStream.pipe(res);
+// }
 // do some error checking to make sure a video exists and if not return a 404
 
-export default function handler(
+// Modify your getVideos API route:
+
+const prisma = new PrismaClient();
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const method = req.method;
 
-  if (method === "GET") {
-    return getVideoStream(req, res);
+  // if (method === "GET") {
+  //   return getVideoStream(req, res);
+  // }
+
+  // if (req.method !== "GET") {
+  //   return res.status(405).json({ message: "Method not allowed" });
+  // }
+
+  // const page = Number(req.query.page) || 1;
+  // const limit = Number(req.query.limit) || 10;
+
+  // const videos = await prisma.video.findMany({
+  //   skip: (page - 1) * limit,
+  //   take: limit,
+  // });
+
+  // res.status(200).json(videos);
+
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed" });
   }
+
+  const { id } = req.query;
+
+  if (!id) {
+    return res.status(400).json({ message: "Missing video ID" });
+  }
+
+  const video = await prisma.video.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!video) {
+    return res.status(404).json({ message: "Video not found" });
+  }
+
+  const videoUrl =
+    `https://res.cloudinary.com/your-cloud-name/video/upload/${video.videoId}.mp4`;
+
+  const videoStream = await fetch(videoUrl);
+
+  res.setHeader("Content-Type", "video/mp4");
+  videoStream.body.pipe(res);
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const videos = await prisma.video.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  res.status(200).json(videos);
+
   if (method === "POST") {
     return uploadVideoStream(req, res);
   }
