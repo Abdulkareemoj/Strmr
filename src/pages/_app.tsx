ab400e601305f64ac36e27d15a5936f21d17f77c
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-21 09:21:54 +0100

    fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
+import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
 
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "~/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
-}
+};
+
+export default MyApp;
