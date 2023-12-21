09c4eff6df182cef36b78e5d5633674145ee9122
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-21 09:06:58 +0100

    more configs

--- app.d.ts
+++
@@ -1,7 +0,0 @@
-// app.d.ts
-/// <reference types="lucia" />
-declare namespace Lucia {
-  type Auth = import("./auth/lucia").Auth;
-  type DatabaseUserAttributes = {};
-  type DatabaseSessionAttributes = {};
-}
