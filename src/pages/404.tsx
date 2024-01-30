4c9f57dd5eec169cb79ec6ba9554f2a83f245bb4
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-01-30 11:03:44 +0100

    typo

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <div className="h-[calc(100vh-100px)] flex flex-col items-center justify-center text-center">
+    <div className="flex h-[calc(100vh-100px)] flex-col items-center justify-center text-center">
       <h1 className="text-5xl font-black">404</h1>
-      <p className="text-4xl font-extrabold">Page not be found</p>
+      <p className="text-4xl font-extrabold">Page not found</p>
       <Link href="/" className="mt-4 text-blue-500 hover:underline">
         Go home
       </Link>
