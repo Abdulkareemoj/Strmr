f4f43f2b8868f226e67712b1fba73098bed04ee0
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-03-26 15:37:15 +0100

    plyr working

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -15,78 +15,105 @@
 //   };
 // };
 
+// import "@vidstack/react/player/styles/base.css";
+
+// import { useEffect, useRef } from "react";
+
+// import {
+//   isHLSProvider,
+//   MediaPlayer,
+//   MediaProvider,
+//   Poster,
+//   Track,
+//   type MediaCanPlayDetail,
+//   type MediaCanPlayEvent,
+//   type MediaPlayerInstance,
+//   type MediaProviderAdapter,
+//   type MediaProviderChangeEvent,
+// } from "@vidstack/react";
+
+// import { VideoLayout } from "~/components/ui/vidstack/layouts/video-layout";
+// // import { textTracks } from "./tracks";
+
+// export default function Player() {
+//   let player = useRef<MediaPlayerInstance>(null);
+
+//   useEffect(() => {
+//     // Subscribe to state updates.
+//     return player.current!.subscribe(({ paused, viewType }) => {
+//       // console.log('is paused?', '->', state.paused);
+//       // console.log('is audio view?', '->', state.viewType === 'audio');
+//     });
+//   }, []);
+
+//   function onProviderChange(
+//     provider: MediaProviderAdapter | null,
+//     nativeEvent: MediaProviderChangeEvent,
+//   ) {
+//     // We can configure provider's here.
+//     if (isHLSProvider(provider)) {
+//       provider.config = {};
+//     }
+//   }
+
+//   // We can listen for the `can-play` event to be notified when the player is ready.
+//   function onCanPlay(
+//     detail: MediaCanPlayDetail,
+//     nativeEvent: MediaCanPlayEvent,
+//   ) {
+//     // ...
+//   }
+
+//   return (
+//     <MediaPlayer
+//       className="ring-media-focus aspect-video w-full overflow-hidden rounded-md bg-slate-900 font-sans text-white data-[focus]:ring-4"
+//       title="Sprite Fight"
+//       src="https://stream.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/low.mp4"
+//       crossorigin
+//       playsinline
+//       onProviderChange={onProviderChange}
+//       onCanPlay={onCanPlay}
+//       ref={player}
+//     >
+//       <MediaProvider>
+//         <Poster
+//           className="absolute inset-0 block h-full w-full rounded-md object-cover opacity-0 transition-opacity data-[visible]:opacity-100"
+//           src="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/thumbnail.webp?time=268&width=1200"
+//           alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
+//         />
+//         {/* {textTracks.map((track) => (
+//           <Track {...track} key={track.src} />
+//         ))} */}
+//       </MediaProvider>
+
+//       <VideoLayout thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt" />
+//     </MediaPlayer>
+//   );
+// }
+
 import "@vidstack/react/player/styles/base.css";
-
-import { useEffect, useRef } from "react";
-
+import "@vidstack/react/player/styles/plyr/theme.css";
+import { MediaPlayer, MediaProvider } from "@vidstack/react";
 import {
-  isHLSProvider,
-  MediaPlayer,
-  MediaProvider,
-  Poster,
-  Track,
-  type MediaCanPlayDetail,
-  type MediaCanPlayEvent,
-  type MediaPlayerInstance,
-  type MediaProviderAdapter,
-  type MediaProviderChangeEvent,
-} from "@vidstack/react";
+  PlyrLayout,
+  plyrLayoutIcons,
+} from "@vidstack/react/player/layouts/plyr";
 
-import { VideoLayout } from "~/components/ui/vidstack/layouts/video-layout";
-// import { textTracks } from "./tracks";
-
-export default function Player() {
-  let player = useRef<MediaPlayerInstance>(null);
-
-  useEffect(() => {
-    // Subscribe to state updates.
-    return player.current!.subscribe(({ paused, viewType }) => {
-      // console.log('is paused?', '->', state.paused);
-      // console.log('is audio view?', '->', state.viewType === 'audio');
-    });
-  }, []);
-
-  function onProviderChange(
-    provider: MediaProviderAdapter | null,
-    nativeEvent: MediaProviderChangeEvent,
-  ) {
-    // We can configure provider's here.
-    if (isHLSProvider(provider)) {
-      provider.config = {};
-    }
-  }
-
-  // We can listen for the `can-play` event to be notified when the player is ready.
-  function onCanPlay(
-    detail: MediaCanPlayDetail,
-    nativeEvent: MediaCanPlayEvent,
-  ) {
-    // ...
-  }
-
+export default function Test() {
   return (
-    <MediaPlayer
-      className="ring-media-focus aspect-video w-full overflow-hidden rounded-md bg-slate-900 font-sans text-white data-[focus]:ring-4"
-      title="Sprite Fight"
-      src="https://stream.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/low.mp4"
-      crossorigin
-      playsinline
-      onProviderChange={onProviderChange}
-      onCanPlay={onCanPlay}
-      ref={player}
-    >
-      <MediaProvider>
-        <Poster
-          className="absolute inset-0 block h-full w-full rounded-md object-cover opacity-0 transition-opacity data-[visible]:opacity-100"
-          src="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/thumbnail.webp?time=268&width=1200"
-          alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
-        />
-        {/* {textTracks.map((track) => (
-          <Track {...track} key={track.src} />
-        ))} */}
-      </MediaProvider>
-
-      <VideoLayout thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt" />
-    </MediaPlayer>
+    <>
+      <main>
+        <MediaPlayer
+          title="Sprite Fight"
+          src="https://stream.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/low.mp4"
+        >
+          <MediaProvider />
+          <PlyrLayout
+            thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt"
+            icons={plyrLayoutIcons}
+          />
+        </MediaPlayer>
+      </main>
+    </>
   );
 }
