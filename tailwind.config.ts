ad80bd1f6d15779da0037020272f4f62b63df645
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-11-12 16:22:34 +0100

    refactor: Replace CommonJS with ES module syntax in configuration files and enhance environment variable management

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -73,4 +75,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+} satisfies Config;
