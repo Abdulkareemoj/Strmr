2c8d5ca224f3ab1db02fa523c254e23612ee1ddd
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-12-13 10:06:41 +0000

    commented out so i can deploy

--- src/pages/upload/video.tsx
+++ src/pages/upload/video.tsx
@@ -1,10 +1,10 @@
 // import UploadComponent from "~/components/UploadComponent";
 
-// export default function VideoUploadPage() {
-//   return (
-//     <div className="container mx-auto p-4">
-//       <h1 className="mb-4 text-2xl font-bold">Upload Videos</h1>
-//       <UploadComponent uploadType="videos" maxFileSize={100} />
-//     </div>
-//   );
-// }
+export default function VideoUploadPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="mb-4 text-2xl font-bold">Upload Videos</h1>
+      {/* <UploadComponent uploadType="videos" maxFileSize={100} /> */}
+    </div>
+  );
+}
